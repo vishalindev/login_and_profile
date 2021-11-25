3291b45249deb8e704b7140dd701a7be606d0a0e
vishal7x <>
2021-11-25 17:17:18 +0000

    New product added

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,76 +1,42 @@
 const products = [
-  {
-    name: 'Airpods Wireless Bluetooth Headphones',
-    image: '/images/Eye.jpg',
-    description:
-      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
-    brand: 'Apple',
-    category: 'Electronics',
-    price: 89.99,
-    countInStock: 3,
-    rating: 0,
-    numReviews: 0,  
+  
+   {
+    name: 'Dettol Antiseptic Liquid Bottle',
+    image: '/images/Dettol.jpg',
+    description:'One of the safest and trusted brands in hygiene is Dettol. This Dettol Hand Sanitizer has an antimicrobial effect. ',
+    brand: 'DETTOL',
+    category: 'Antiseptic Liquid',
+    price: 129.99,
+    countInStock: 400,
+    rating: 930,
+    numReviews: 1224,
   },
-  {
-    name: 'iPhone 11 Pro 256GB Memory',
-    image: '/images/phone.jpg',
-    description:
-      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
-    brand: 'Apple',
-    category: 'Electronics',
-    price: 599.99,
-    countInStock: 10,
-    rating: 0,
-    numReviews: 0,
+
+{
+    name: 'Calcimax',
+    image: '/images/Calcimax.jpg',
+    description:'PharmEasy Hot Water Bag is specially designed to provide a hot compress that can help to relieve pain when applied locally. It is made with high-quality rubber that is not affected by hot water.',
+    brand: 'Calcimax',
+    category: 'Vitamine',
+    price: 210.00,
+    countInStock:10,
+    rating: 3,
+    numReviews: 4,
   },
-  {
-    name: 'Cannon EOS 80D DSLR Camera',
-    image: '/images/camera.jpg',
-    description:
-      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
-    brand: 'Cannon',
-    category: 'Electronics',
-    price: 929.99,
-    countInStock: 0,
-    rating: 0,
-    numReviews: 0,
+{
+    name: 'Protinex',
+    image: '/images/Protinex.jpg',
+    description:'The daily meal you eat may often give you only half the nutrition required by your body to lead an active life.',
+    brand: 'Protinex',
+    category: 'Immunity Booster',
+    price: 400.99,
+    countInStock: 100,
+    rating: 120,
+    numReviews: 210,
   },
-  {
-    name: 'Sony Playstation 4 Pro White Version',
-    image: '/images/playstation.jpg',
-    description:
-      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
-    brand: 'Sony',
-    category: 'Electronics',
-    price: 399.99,
-    countInStock: 10,
-    rating: 0,
-    numReviews: 0,
-  },
-  {
-    name: 'Logitech G-Series Gaming Mouse',
-    image: '/images/mouse.jpg',
-    description:
-      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
-    brand: 'Logitech',
-    category: 'Electronics',
-    price: 49.99,
-    countInStock: 7,
-    rating: 0,
-    numReviews: 0,
-  },
-  {
-    name: 'Amazon Echo Dot 3rd Generation',
-    image: '/images/alexa.jpg',
-    description:
-      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
-    brand: 'Amazon',
-    category: 'Electronics',
-    price: 29.99,
-    countInStock: 0,
-    rating: 0,
-    numReviews: 0,
-  },
+
+
+
 ]
 
 export default products
